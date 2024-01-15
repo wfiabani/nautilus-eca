05063c9e5aa4095e5e37fc94107a4e381614c045
Wilian Fiabani <arquitetura@santrack.com.br>
2024-01-14 21:43:11 -0300

    Adicionada jsoup lib

--- customer-service/src/main/java/br/com/nautilus/controller/CustomerController.java
+++ customer-service/src/main/java/br/com/nautilus/controller/CustomerController.java
@@ -1,10 +1,19 @@
 package br.com.nautilus.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -48,5 +57,93 @@ public class CustomerController {
 	public String getCustomerServiceVersion(){
 		return String.format(template, config.getCustomerServiceVersion());
 	}
+	
+	/*
+	@GetMapping(value = "/jogo-do-bixo")
+	public String jogoDoBixo() {
+		try {
+            
+            URL url = new URL("https://www.ojogodobicho.com/deu_no_poste.htm");
+             
+            // read text returned by server
+            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+             
+            String line;
+            String all = "";
+            while ((line = in.readLine()) != null) {
+                all = all.concat(line);
+            }
+            in.close();
+            
+            org.jsoup.nodes.Document doc = Jsoup.parse(all);
+            Elements tbody = doc.select("tbody");
+            //System.out.println(tbody.first());
+            
+            // elements
+            List<String> results = new ArrayList<String>();
+            results.add(
+            		"PPT;"
+            		+tbody.first().child(0).child(1).text()+";"
+            		+tbody.first().child(1).child(1).text()+";"
+            		+tbody.first().child(2).child(1).text()+";"
+            		+tbody.first().child(3).child(1).text()+";"
+            		+tbody.first().child(4).child(1).text()+";"
+            );
+            results.add(
+            		"PTM;"
+            		+tbody.first().child(0).child(2).text()+";"
+            		+tbody.first().child(1).child(2).text()+";"
+            		+tbody.first().child(2).child(2).text()+";"
+            		+tbody.first().child(3).child(2).text()+";"
+            		+tbody.first().child(4).child(2).text()+";"
+            );
+            results.add(
+            		"PT;"
+            		+tbody.first().child(0).child(3).text()+";"
+            		+tbody.first().child(1).child(3).text()+";"
+            		+tbody.first().child(2).child(3).text()+";"
+            		+tbody.first().child(3).child(3).text()+";"
+            		+tbody.first().child(4).child(3).text()+";"
+            );
+            results.add(
+            		"PTV;"
+            		+tbody.first().child(0).child(4).text()+";"
+            		+tbody.first().child(1).child(4).text()+";"
+            		+tbody.first().child(2).child(4).text()+";"
+            		+tbody.first().child(3).child(4).text()+";"
+            		+tbody.first().child(4).child(4).text()+";"
+            );
+            results.add(
+            		"PTN;"
+            		+tbody.first().child(0).child(5).text()+";"
+            		+tbody.first().child(1).child(5).text()+";"
+            		+tbody.first().child(2).child(5).text()+";"
+            		+tbody.first().child(3).child(5).text()+";"
+            		+tbody.first().child(4).child(5).text()+";"
+            );
+            results.add(
+            		"COR;"
+            		+tbody.first().child(0).child(6).text()+";"
+            		+tbody.first().child(1).child(6).text()+";"
+            		+tbody.first().child(2).child(6).text()+";"
+            		+tbody.first().child(3).child(6).text()+";"
+            		+tbody.first().child(4).child(6).text()+";"
+            );
+            System.out.println(results);
+            
+            return all;
+             
+        }
+        catch (MalformedURLException e) {
+            System.out.println("Malformed URL: " + e.getMessage());
+        }
+        catch (IOException e) {
+            System.out.println("I/O Error: " + e.getMessage());
+        }
+        return "";
+        
+	}
+	*/
+
 
 }
